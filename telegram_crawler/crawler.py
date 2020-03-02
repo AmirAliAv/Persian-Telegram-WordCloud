75323ddda6b8c96119a601879a77e2f2aac626c2
1997alireza <torabian.alireza@gmail.com>
2020-03-02 21:00:08 +0330

    now a special user's messages can be extracted

--- telegram_crawler/crawler.py
+++ telegram_crawler/crawler.py
@@ -7,27 +7,43 @@ from telethon.tl.types import DocumentAttributeAudio
 from telethon.tl.types import Message
 
 import sys, traceback
+import asyncio
+
+# params
+MAX_MSG_COUNT = 100
 
 
 class Crawler:
-    def __init__(self, dialog: Dialog, client: TelegramClient):
+    def __init__(self, dialog: Dialog, client: TelegramClient, target_identifier):
         super().__init__()
         self.dialog = dialog
         self.client = client
+        self.targetId = self.get_user_entity(target_identifier).id
 
-    def go(self):
+    def get_user_entity(self, identifier):
+        return asyncio.get_event_loop().run_until_complete(self.client.get_entity(identifier))
+
+    def extract_messages_body(self):
+        messages_text = []
+        chat_name = self.dialog.name
+        print('Chat Name: ', chat_name)
+
+        i = 0
         for message in self.client.iter_messages(self.dialog, limit=None):
+            if i >= MAX_MSG_COUNT:
+                break
             if message.message is None or len(message.message) == 0:
                 continue
             message_id = message.id
             body = message.message
             from_id = message.from_id
+            if from_id == self.targetId:
+                i += 1
+                messages_text.append(body)
             fwd_from = message.fwd_from
             if fwd_from is not None:
                 fwd_from = fwd_from.from_id
             reply_to_msg_id = message.reply_to_msg_id
             date = message.date
-            chat_name = self.dialog.name
 
-            print(body)
-            # TODO: return messages' bodies
+        return messages_text
--- telegram_crawler/main.py
+++ telegram_crawler/main.py
@@ -9,21 +9,39 @@ async def get_dialogs(connector):
 
 
 def extract_dialogs_from_telegram_server():
+    # params
+    target_identifier = '-'
+    candidate_dialog_names = ['-']
+    max_dialog_count = 20  # if candidate_dialog_names is empty
+
     c = Connector()
 
     dialogs = asyncio.get_event_loop().run_until_complete(get_dialogs(c))
 
-    print('Dialogs: ' + str(len(dialogs)))
+    messages = []
 
-    n = len(dialogs)
-    i = 0
-    for dialog in dialogs:
-        i += 1
-        # if i > 2: exit()
-        print('dialog {} / {}'.format(i, n))
-        crawler = Crawler(dialog=dialog, client=c.client)
-        crawler.go()
+    print('Number of your dialogs: ' + str(len(dialogs)))
+
+    if len(candidate_dialog_names) == 0:
+        n = min(len(dialogs), max_dialog_count)
+
+        for i in range(n):
+            dialog = dialogs[i]
+            print('dialog {} / {}'.format(i, n))
+            crawler = Crawler(dialog=dialog, client=c.client, target_identifier=target_identifier)
+            messages.extend(crawler.extract_messages_body())
+
+    else:
+        n = len(dialogs)
+        for i in range(n):
+            dialog = dialogs[i]
+            print('dialog {} / {}'.format(i, n))
+            if dialog.name in candidate_dialog_names:
+                crawler = Crawler(dialog=dialog, client=c.client, target_identifier=target_identifier)
+                messages.extend(crawler.extract_messages_body())
+
+    return messages
 
 
 if __name__ == '__main__':
-    extract_dialogs_from_telegram_server()
+    print(extract_dialogs_from_telegram_server())
